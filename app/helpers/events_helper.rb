f4652340773975942c323c59fcb1a7513e463ba1
Maxon Tayebwa <maxtayebwa@gmail.com>
2022-03-28 17:02:57 +0300

    add and use view helpers

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,10 @@
 module EventsHelper
+  def price(event)
+    return "Free" if event.free?
+    number_to_currency(event.price, precision: 0)
+  end
+
+  def format_time(event)
+    event.starts_at.strftime("%B %d at %I:%M %P")
+  end
 end
