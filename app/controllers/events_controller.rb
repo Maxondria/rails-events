5542b2b0b87a25da17b22ca6ad364c31ba2be12f
Maxon Tayebwa <maxtayebwa@gmail.com>
2022-03-29 11:34:06 +0300

    ability to update an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,22 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    event_params = params
+      .require(:event)
+      .permit(:name, :description, :location, :price, :starts_at)
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit # render the edit template of the same controller
+    end
+  end
 end
