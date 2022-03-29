04c8ed7fbb2a37fc20864ca744b5ca9a1d78e7e7
Maxon Tayebwa <maxtayebwa@gmail.com>
2022-03-29 13:59:56 +0300

    ability to create new event + breakout the view into partials

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,14 +14,32 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
 
-    event_params = params
-      .require(:event)
-      .permit(:name, :description, :location, :price, :starts_at)
-
     if @event.update(event_params)
       redirect_to @event
     else
       render :edit # render the edit template of the same controller
     end
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new # render the new template of the same controller
+    end
+  end
+
+  private
+
+  def event_params
+    params
+      .require(:event)
+      .permit(:name, :description, :location, :price, :starts_at)
   end
 end
