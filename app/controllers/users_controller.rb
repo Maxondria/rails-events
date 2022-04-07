de6caf47ceb2bacc91e8c28c5d23fa3ab9cf7233
Maxon Tayebwa <maxtayebwa@gmail.com>
2022-04-07 11:05:07 +0300

    add ability to delete user and edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :destroy, :update]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
+    else
+      render :edit
+    end
   end
 
   def create
@@ -21,11 +33,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    if @user.destroy
+      redirect_to events_url, danger: "Account was successfully deleted."
+    else
+      flash[:alert] = "There was a problem deleting your account."
+      render :show
+    end
+  end
+
   private
 
   def user_params
     params
       .require(:user)
       .permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
