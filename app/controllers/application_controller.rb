8f1f7b7df2d70e1ee809b9eaeed7d0e2c1036745
Maxon Tayebwa <maxtayebwa@gmail.com>
2022-04-07 23:38:56 +0300

    implement require_sign to prevent user actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   # Add a custom flash key to the flash hash
   add_flash_types :danger
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # declares this method as part of the view helper methods
+  helper_method :current_user
+
+  def require_signin
+    unless current_user
+      session[:previous_url] = request.url
+      redirect_to signin_path, alert: "Please sign in first!"
+    end
+  end
 end
